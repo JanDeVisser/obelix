ab3a919f2ccccdbebf6bcafdecd89d5fd3fab4ac
Jan de Visser <jan@de-visser.net>
2015-02-27 16:38:10 -0500

    xx

---
+++ src/stdlib/builtins.c
@@ -0,0 +1,45 @@
+/*
+ * /obelix/src/script.c - Copyright (c) 2014 Jan de Visser <jan@finiandarcy.com>
+ *
+ * This file is part of obelix.
+ *
+ * obelix is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * obelix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with obelix.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <array.h>
+#include <data.h>
+#include <dict.h>
+
+extern data_t * _function_print(char *, array_t *, dict_t *);
+
+data_t * _function_print(char *name, array_t *params, dict_t *kwargs) {
+  data_t        *value;
+
+  assert(array_size(params));
+  value = (data_t *) array_get(params, 0);
+  assert(value);
+
+  /*
+   * Ideally we want to do a printf-style function. The idea is to have the
+   * first argument as the format string, and following parameters as values.
+   * Since there is no API for sprintf that takes a user-accessible data
+   * structure, just the va_list one, we will have to go through the format
+   * string and do it one by one.
+   *
+   * But that's future. Now we just print the first parameter :-)
+   */
+  printf("%s\n", data_tostring(value));
+  return data_create(Int, 0);
+}
+
