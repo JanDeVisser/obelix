04fead3ca96693e684e79a9fb4a73693aabbe507
Jan de Visser <jan@de-visser.net>
2021-10-26 10:35:29 -0400

    Generate config.h from the config.in.h template, and include project source in include path so it can be included.

---
+++ config.h
@@ -0,0 +1,27 @@
+/*
+ * config.h.in - Copyright (c) 2015 Jan de Visser <jan@finiandarcy.com>
+ *
+ * This file is part of Obelix.
+ *
+ * Obelix is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Obelix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Obelix.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define OBELIX_NAME "Obelix"
+#define OBELIX_VERSION_MAJOR "1"
+#define OBELIX_VERSION_MINOR "0"
+#define OBELIX_VERSION "1.0"
+#define OBELIX_DIR "/Users/jan/projects/obelix2/cmake-build-debug"
+#define OBELIX_DATADIR "/Users/jan/projects/obelix2/cmake-build-debug/share"
