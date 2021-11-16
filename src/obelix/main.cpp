595770278fa9dd76b46948ab74f8b606a362786a
Jan de Visser <jan@de-visser.net>
2021-11-16 15:02:24 -0500

    Start of the REPL.

--- src/obelix/main.cpp
+++ src/obelix/main.cpp
@@ -5,10 +5,53 @@
 #include <obelix/Runtime.h>
 #include <obelix/Scope.h>
 #include <optional>
-#include <vector>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
 
 namespace Obelix {
 
+class Repl {
+public:
+    explicit Repl(Runtime &runtime)
+        : m_runtime(runtime)
+    {
+    }
+
+    int run()
+    {
+        Parser parser(m_runtime.config());
+        printf("Obelix 1.99 - A programming language\n");
+        bool quit = false;
+        auto scope = m_runtime.new_scope();
+        do {
+            auto line = readline(m_prompt.c_str());
+            if (!line || !strcmp(line, "exit")) {
+                quit = true;
+            } else {
+                auto tree = parser.parse(m_runtime, line);
+                for (auto& e : parser.errors()) {
+                    printf("ERROR: %s\n", e.to_string().c_str());
+                }
+                if (tree) {
+                    printf("%s\n", tree->to_string(0).c_str());
+                    if (auto result = tree->execute_in(scope); result.code == ExecutionResultCode::Error) {
+                        printf("ERROR: %s\n", result.return_value->to_string().c_str());
+                    } else if (result.return_value){
+                        printf("%s\n", result.return_value->to_string().c_str());
+                    }
+                    scope = tree->scope();
+                }
+            }
+        } while (!quit);
+	return 0;
+    }
+
+private:
+    Runtime& m_runtime;
+    std::string m_prompt { "> "};
+};
 
 }
 
@@ -38,11 +81,12 @@ int main(int argc, char** argv)
         }
     }
 
+    auto runtime = Obelix::Runtime(config);
+
     if (file_name.empty()) {
-        usage();
+        return Obelix::Repl(runtime).run();
     }
 
-    auto runtime = Obelix::Runtime(config);
     auto result = runtime.run(file_name);
 
     if (auto exit_code = result.return_value; exit_code) {
