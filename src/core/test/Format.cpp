f13e62511cc13259b3971a9ddc03a272568f6d11
Jan de Visser <jan@de-visser.net>
2021-12-23 08:24:12 -0500

    Hex tests

--- src/core/test/Format.cpp
+++ src/core/test/Format.cpp
@@ -4,10 +4,6 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-//
-// Created by Jan de Visser on 2021-09-22.
-//
-
 #include <core/Format.h>
 #include <core/StringUtil.h>
 #include <gtest/gtest.h>
@@ -39,4 +35,16 @@ TEST(Format, format_escape)
 {
     std::string formatted = Obelix::format("String: '{}' Escaped brace: {{ and a close } int: {}-", "Hello World!", 42);
     EXPECT_EQ(formatted, "String: 'Hello World!' Escaped brace: { and a close } int: 42-");
+}
+
+TEST(Format, format_hex)
+{
+    std::string formatted = Obelix::format("{x}", 0x42);
+    EXPECT_EQ(formatted, "42");
+}
+
+TEST(Format, format_hex_width)
+{
+    std::string formatted = Obelix::format("{04x}", 0x42);
+    EXPECT_EQ(formatted, "0042");
 }
