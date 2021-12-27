86dd0f7ce70d2861f96bb98826fb8e8521a29d5a
Jan de Visser <jan@de-visser.net>
2021-12-27 15:12:40 -0500

    Added RSP register and pushed DEREFCONTROLLER outside the 0-15 range

--- src/jv80/cpu/registers.h
+++ src/jv80/cpu/registers.h
@@ -9,7 +9,6 @@
 namespace Obelix::JV80::CPU {
 
 constexpr static int GP_A = 0;
-constexpr static int RSP = 0;
 constexpr static int GP_B = 1;
 constexpr static int GP_C = 2;
 constexpr static int GP_D = 3;
@@ -17,13 +16,16 @@ constexpr static int LHS = 4;
 constexpr static int RHS = 5;
 constexpr static int IR = 6;
 constexpr static int MEM = 7;
+
 constexpr static int PC = 8;
 constexpr static int SP = 9;
-constexpr static int SI = 10;
-constexpr static int DI = 11;
-constexpr static int TX = 12;
-constexpr static int CONTROLLER = 13;
-constexpr static int DEREFCONTROLLER = 14;
+constexpr static int RSP = 10;
+constexpr static int SI = 11;
+constexpr static int DI = 12;
+constexpr static int TX = 13;
+constexpr static int CONTROLLER = 14;
 constexpr static int MEMADDR = 15;
+
+constexpr static int DEREFCONTROLLER = 16;
 
 }
