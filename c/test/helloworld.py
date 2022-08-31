1a2ff89426f5e9608afbf433845dfa8a94038fb0
Jan de Visser <jan@de-visser.net>
2022-08-31 10:16:58 -0400

    Deleted old C code

--- c/test/helloworld.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-print("Hello %s!" % "Asterix")
-
-print >> sys.stdout, "Hello from stdout, %s!" % "Asterix"
-print >> sys.stderr, "Hello from stderr, %s!" % "Asterix"
-
--- c/test/oneplusone.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-
-exit(1 + 1)
-
