f0ab11cf4f2b215e0f6a762eb0ebdae644dd7e00
Jan de Visser <jan@de-visser.net>
2022-09-05 11:37:50 -0400

    Null pointer handling didn't work as designed

--- src/rt/arch/arm64/puts.s
+++ src/rt/arch/arm64/puts.s
@@ -20,25 +20,24 @@ puts:
     mov     fp,sp
 
     cmp     x1,#0
-    b.ne    __puts_print
-
-    ; Print '[[null]]' if the buffer is the null pointer:
-    adr     x1,__str_nullptr
-    adr     x9,__str_nullptr_len
-    ldr     x2,[x9]
+    b.eq    __puts_print_null
+    mov     x2,x0
 
     ; Print the string we generated using to_string:
 __puts_print:
-    mov     x2,x0
     mov     x0,#1
     mov     x16,#0x04
     svc     #0x00
     ldp     fp,lr,[sp],#16
     ret
 
+    ; Print '[[null]]' if the buffer is the null pointer:
+__puts_print_null:
+    adr     x1,__str_nullptr
+    mov     x2,str_nullptr_len
+    b       __puts_print
+
 __str_nullptr:
     .string "[[null]]"
 
-.align 4
-__str_nullptr_len:
-    .int    9
+.equ str_nullptr_len,9
